d759c85c516fa8ccaa41875bdc85abe5e9a6a7ff
aleikson <aleiksonsilva@hotmail.com>
2023-07-17 22:43:45 -0300

    Added Router

--- src/App.js
+++ src/App.js
@@ -1,11 +1,20 @@
 import './App.css';
 import NavBar from './components/navbar/NavBar';
+import Home from './components/home/Home'
+import CaseObject from './components/case/CaseObject';
+import { Route, Routes, BrowserRouter } from "react-router-dom";
 
 function App() {
   return (
+    <BrowserRouter>
     <div className="App"> 
         <NavBar />
+    <Routes>
+        <Route path='/' element={<Home />} />
+        <Route path='/case' element={<CaseObject />} />
+    </Routes>
     </div>
+    </BrowserRouter>
   );
 }
 
