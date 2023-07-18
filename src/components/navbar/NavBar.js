8852c92a4998bac9377046ce87a686b8dcb11894
aleikson <aleiksonsilva@hotmail.com>
2023-07-17 22:44:04 -0300

    Small Changes

--- src/components/navbar/NavBar.js
+++ src/components/navbar/NavBar.js
@@ -1,27 +1,12 @@
 import React, { useState } from 'react';
 import style from './NavBar.module.css';
+import { Link } from "react-router-dom";
 
 const NavBar = () => {
   const [isMenuOpen, setMenuOpen] = useState(false);
 
   const toggleMenu = () => {
     setMenuOpen(!isMenuOpen);
-  };
-
-  const handleLinkClick = (e) => {
-    e.preventDefault();
-
-    const targetId = e.target.getAttribute('href').substring(1);
-    const targetElement = document.getElementById(targetId);
-
-    if (targetElement) {
-      window.scrollTo({
-        top: targetElement.offsetTop,
-        behavior: 'smooth',
-      });
-    }
-
-    setMenuOpen(false);
   };
 
   return (
@@ -31,9 +16,9 @@ const NavBar = () => {
         <span>&#124; front end developer</span>
       </a>
       <div className={`${style.links} ${isMenuOpen ? style.open : style.closed}`}>
-        <a href="#about" onClick={handleLinkClick}>About</a>
-        <a href="#case" onClick={handleLinkClick}>Case Studies</a>
-        <a href="#contact" onClick={handleLinkClick}>Contact</a>
+        <Link to='/caseStudies'>About</Link>
+        <Link to='/case'>Case Studies</Link>
+        <a href="#contact">Contact</a>
       </div>
       <div className={style.menuIcon} onClick={toggleMenu}>
         <span className={`${style.iconBar} ${isMenuOpen ? style.open : ''}`} />
