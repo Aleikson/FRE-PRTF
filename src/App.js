8697cfa442f75bee33d562252ce1f3b716b5fed2
aleikson <aleiksonsilva@hotmail.com>
2023-07-23 14:29:58 -0300

    Removed route

--- src/App.js
+++ src/App.js
@@ -1,22 +1,19 @@
 import './App.css';
 import NavBar from './components/navbar/NavBar';
 import Home from './components/home/Home'
-import { Route, Routes, BrowserRouter } from "react-router-dom";
 import CaseObject from './components/case/CaseObject';
 import Capabilities from './components/capabilities/Capabilities'
+import Contact from './components/contact/Contact';
 
 function App() {
   return (
-    <BrowserRouter>
-    <div className="App"> 
-        <NavBar />
-    <Routes>
-        <Route path='/' element={<Home />} />   
-    </Routes>
-    <CaseObject />
-    <Capabilities />
+    <div className="App">
+      <NavBar />
+      <Home />
+      <CaseObject />
+      <Capabilities />
+      <Contact />
     </div>
-    </BrowserRouter>
   );
 }
 
--- src/components/navbar/NavBar.js
+++ src/components/navbar/NavBar.js
@@ -1,6 +1,5 @@
 import React, { useState } from 'react';
 import style from './NavBar.module.css';
-import { Link } from "react-router-dom";
 
 const NavBar = () => {
   const [isMenuOpen, setMenuOpen] = useState(false);
@@ -13,11 +12,11 @@ const NavBar = () => {
     <div className={style.container}>
       <a href="/" className={style.nameLink}>
         <h1>Aleikson Silva</h1>
-        <span>&#124; front end developer</span>
+        <span>&#124; front-end developer</span>
       </a>
       <div className={`${style.links} ${isMenuOpen ? style.open : style.closed}`}>
-        <Link to='/caseStudies'>About</Link>
-        <Link to='/case'>Case Studies</Link>
+        <a href="#about">About</a>
+        <a href="#case">Case Studies</a>
         <a href="#contact">Contact</a>
       </div>
       <div className={style.menuIcon} onClick={toggleMenu}>
