007e037e7870c89bed0e4108e4a4f91f96fb0e50
aleikson <aleiksonsilva@hotmail.com>
2023-07-27 00:43:08 -0300

    Added footer

---
+++ src/components/footer/Footer.js
@@ -0,0 +1,34 @@
+import React from 'react';
+import { FaEnvelope, FaGithub } from 'react-icons/fa';
+import { BsLinkedin } from 'react-icons/bs';
+import Style from './Footer.module.css';
+
+const Footer = () => {
+  const currentYear = new Date().getFullYear();
+  const authorName = 'Aleikson';
+
+  return (
+    <div className={Style.footerContainer}>
+      <div className={Style.contactContainer}>
+        <div className={Style.content}>
+          <a href="mailto:aleiksonsilva@hotmail.com">
+            <FaEnvelope className={Style.contactIcon} />
+          </a>
+        </div>
+        <div className={Style.content}>
+          <a href="https://www.linkedin.com/in/aleikson-frontend/">
+            <BsLinkedin className={Style.contactIcon} />
+          </a>
+        </div>
+        <div className={Style.content}>
+          <a href="https://github.com/Aleikson">
+            <FaGithub className={Style.contactIcon} />
+          </a>
+        </div>
+      </div>
+      <p className={Style.footerText}>&copy; {currentYear} {authorName} - Todos os direitos reservados</p>
+    </div>
+  );
+};
+
+export default Footer;
