591f8cbbc3b2f12ee141a547d674a47211af19c2
aleikson <aleiksonsilva@hotmail.com>
2023-08-14 19:59:25 -0300

    Added Studies & Removed Case

--- src/App.js
+++ src/App.js
@@ -1,17 +1,17 @@
 import './App.css';
 import NavBar from './components/navbar/NavBar';
 import Home from './components/home/Home'
-import CaseObject from './components/case/CaseObject';
 import Capabilities from './components/capabilities/Capabilities'
 import Contact from './components/contact/Contact';
 import Footer from './components/footer/Footer'
+import Studies from './components/studies/Studies';
 
 function App() {
   return (
     <div className="App">
       <NavBar />
       <Home />
-      <CaseObject />
+      <Studies />
       <Capabilities />
       <Contact />
       <Footer />
