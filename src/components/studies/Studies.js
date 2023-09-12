fc5a4d7ea996b62e4c1b2c4846cff742b7097d2e
aleikson <aleiksonsilva@hotmail.com>
2023-09-12 08:37:31 -0300

    Fixed: data.map

--- src/components/studies/Studies.js
+++ src/components/studies/Studies.js
@@ -1,9 +1,8 @@
-import React, { useState } from 'react';
+import React from 'react';
 import Style from './Studies.module.css';
 import project1 from '../../assets/1.png';
 import project2 from '../../assets/2.png';
 import project3 from '../../assets/3.png';
-import fullProject3 from '../../assets/fullProject3.png'; // Import the additional image
 
 const data = [
   {
@@ -19,69 +18,11 @@ const data = [
   {
     id: 3,
     image: project3,
-    additionalImage: fullProject3,
-    fullImageLink: fullProject3, // Link to open the full image in a new tab
+    link: ''
   },
 ];
 
-const Tile = ({ data }) => {
-  const [hover, setHover] = useState(false);
-  const [open, setOpen] = useState(false);
-
-  const handleMouseEnter = () => {
-    setHover(true);
-  };
-
-  const handleMouseLeave = () => {
-    setHover(false);
-  };
-
-  const handleClick = () => {
-    if (data.fullImageLink) {
-      window.open(data.fullImageLink, '_blank');
-    } else {
-      setOpen((prevState) => !prevState);
-    }
-  };
-
-  const tileStyle = {
-    width: open ? '62vw' : '25vw',
-    height: open ? '62vw' : '25vw',
-    position: open ? 'absolute' : 'static',
-    top: open ? '50%' : 'auto',
-    left: open ? '50%' : 'auto',
-    marginTop: open ? '-31vw' : '0',
-    marginLeft: open ? '-31vw' : '0',
-    boxShadow: open ? '0 0 40px 5px rgba(0, 0, 0, 0.3)' : 'none',
-    transform: open ? 'none' : hover ? 'scale(1.1)' : 'none',
-  };
-
-  const content = (
-    <div id='case' className={Style.content} onClick={handleClick}>
-      {open && data.additionalImage ? (
-        <img src={data.additionalImage} alt={`Full Project`} />
-      ) : (
-        <a
-          href={data.link}
-          target={data.fullImageLink ? '_blank' : '_self'}
-          rel={data.fullImageLink ? 'noopener noreferrer' : ''}
-        >
-          <img
-            onMouseEnter={handleMouseEnter}
-            onMouseLeave={handleMouseLeave}
-            src={data.image}
-            alt={`Project ${data.id}`}
-            style={tileStyle}
-          />
-        </a>
-      )}
-    </div>
-  );
-
-  return <div className={Style.tile}>{content}</div>;
-};
-
-const Tiles = ({ data }) => {
+const Studies = () => {
   return (
     <div className={Style.container}>
       <div style={{ textAlign: 'center', margin: '20px' }}>
@@ -90,15 +31,21 @@ const Tiles = ({ data }) => {
       </div>
       <div className={Style.tiles}>
         {data.map((data) => (
-          <Tile data={data} key={data.id} />
+          <div className={Style.tile} key={data.id}>
+            <div id='case' className={Style.content}>
+              <a
+                href={data.link}
+                target='_blank'
+                rel='noopener noreferrer'
+              >
+                <img src={data.image} alt={`Project ${data.id}`} />
+              </a>
+            </div>
+          </div>
         ))}
       </div>
     </div>
   );
-};
-
-const Studies = () => {
-  return <Tiles data={data} />;
 };
 
 export default Studies;
