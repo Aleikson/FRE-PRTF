01f3cdaa2abba6aeb315c1b73b283f56a7d24c0a
Aleikson <aleiksonsilva@hotmail.com>
2023-09-19 21:08:03 -0300

    Small changes

--- src/components/home/Home.js
+++ src/components/home/Home.js
@@ -1,24 +1,19 @@
 import React from 'react';
 import Style from './Home.module.css';
-import Skills from '../skills/Skills'
+
 
 const Home = () => {
   return (
     <div id='home' className={Style.home}>
       <div className={Style.content}>
         <div className={Style.title}>
-          <h1>I’m a Front-End Web Developer & UI designer</h1>
+          <h1>Hello I'm <span>Aleikson Silva</span></h1>
+          <h1>Frontend Developer & UI designer</h1>
         </div>
-        <div className={Style.description}>
-          <p>Experienced in creating responsive and intuitive websites, with a focus on usability and accessibility.</p>
-          <p>Helping people turn their ideas into successful digital projects.</p>
-        </div>
-        <div className={Style.btnContainer}>
-          <a href='#case' className='primary-btn-global'>View my work</a>
-          <a href='#capabilities' className='secondary-btn-global'>more about me</a>
+        <div className={Style.listContainer}>
+          <a href='#studies'>View My Work</a>
         </div>
       </div>
-      <Skills />
     </div >
   );
 };
