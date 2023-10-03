a9d0ece70f31b524eb9d6c803d23401d5e2f9c51
Aleikson <aleiksonsilva@hotmail.com>
2023-10-02 23:34:36 -0300

    New feature for overlay

--- src/components/studies/Studies.js
+++ src/components/studies/Studies.js
@@ -4,7 +4,6 @@ import project1 from '../../assets/1.png';
 import project2 from '../../assets/2.png';
 import project3 from '../../assets/3.png';
 import project4 from '../../assets/4.png'
-import coming from '../../assets/soon.png';
 import project3full from '../../assets/fullProject3.png'
 import project4Full from '../../assets/fullProject4.png'
 
@@ -14,37 +13,29 @@ const data = [
     image: project1,
     link: 'https://cupcake-palace.vercel.app/',
     description: 'High-quality e-commerce for a cupcake bakery.',
+    type: 'Site'
   },
   {
     id: 2,
     image: project2,
     link: 'https://plan-note.vercel.app/',
     description: 'Web application designed to enhance your daily productivity.',
+    type: 'Site'
   },
   {
     id: 3,
     image: project3,
     link: project3full,
     description: 'Modern design for a beachfront hotel.',
+    type: 'Design'
   },
   {
     id: 4,
     image: project4,
     link: project4Full,
-    description: 'Mordern design for your hamburguer restaurant',
-  },
-  {
-    id: 5,
-    image: coming,
-    link: '',
-    description: '',
-  },
-  {
-    id: 6,
-    image: coming,
-    link: '',
-    description: '',
-  },
+    description: 'Modern design for your hamburger restaurant',
+    type: 'Design'
+  }
 ];
 
 const Studies = () => {
@@ -79,7 +70,7 @@ const Studies = () => {
               <p className={Style.overlayDescription}>{hoveredProject === project.id ? hoveredDescription : ''}</p>
               {project.link && (
                 <a href={project.link} target="_blank" rel="noopener noreferrer" className={Style.overlayButton}>
-                  Visit Website
+                  {project.type === 'Site' ? 'Visit Site' : 'See Design'}
                 </a>
               )}
             </div>
